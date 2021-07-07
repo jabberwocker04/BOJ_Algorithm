9db5e4e08feeaf68e4af3ed6808171203e9596a5
0000 <jabberwocker0@gmail.com>
2021-07-07 23:48:25 +0900

    21. 7. 7

---
+++ src/SilverIV/Main_1676.java
@@ -0,0 +1,19 @@
+package SilverIV;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_1676 {
+
+	public static void main(String[] args) throws Exception {// 팩토리얼 0의 개수
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int result = 1;
+		for(int i=1; i<a+1; i++) {
+			result = result*i; // 팩토리얼
+		}
+		
+		System.out.println(result);
+		
+	}
+}
